a1d876456fdcbabf5c9a7fdaecfee67b4e0e21e3
vramaswamy <vramaswamy>
2006-03-23 22:13:35 +0000

    [132367] gmf_head cmcgee 060323 Provide specialized post commit listener that can open a write transaction

--- org.eclipse.gmf.runtime.diagram.core/src/org/eclipse/gmf/runtime/diagram/core/DiagramEditingDomainFactory.java
+++ org.eclipse.gmf.runtime.diagram.core/src/org/eclipse/gmf/runtime/diagram/core/DiagramEditingDomainFactory.java
@@ -14,7 +14,6 @@ package org.eclipse.gmf.runtime.diagram.core;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.core.commands.operations.IOperationHistory;
 import org.eclipse.core.commands.operations.OperationHistoryFactory;
