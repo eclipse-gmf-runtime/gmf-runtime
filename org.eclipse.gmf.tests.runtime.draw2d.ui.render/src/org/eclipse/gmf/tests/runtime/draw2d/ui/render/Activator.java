77b064fce41c23663759bec36aad820a51855698
ldamus <ldamus>
2006-04-11 17:18:07 +0000

    [135527] gmf_head ldamus 060411 Remove usage of org.eclipse.core.runtime.compatibility

---
+++ org.eclipse.gmf.tests.runtime.draw2d.ui.render/src/org/eclipse/gmf/tests/runtime/draw2d/ui/render/Activator.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    IBM Corporation - initial API and implementation 
+ ****************************************************************************/
+package org.eclipse.gmf.tests.runtime.draw2d.ui.render;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+/**
+ * The main plugin class to be used in the desktop.
+ * 
+ * @author Anthony Hunter
+ */
+public class Activator extends AbstractUIPlugin {
+	/**
+	 * Empty block
+	 */
+}
---
+++ org.eclipse.gmf.tests.runtime.emf.commands.core/src/org/eclipse/gmf/tests/runtime/emf/commands/core/Activator.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    IBM Corporation - initial API and implementation 
+ ****************************************************************************/
+package org.eclipse.gmf.tests.runtime.emf.commands.core;
+
+import org.eclipse.core.runtime.Plugin;
+
+
+/**
+ * The main plugin class to be used in the desktop.
+ * 
+ * @author Linda Damus
+ */
+public class Activator extends Plugin {
+	/**
+	 * Empty block
+	 */
+}
