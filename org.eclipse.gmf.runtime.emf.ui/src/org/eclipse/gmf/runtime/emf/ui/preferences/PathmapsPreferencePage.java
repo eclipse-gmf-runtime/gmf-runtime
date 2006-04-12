8ce06fe0be33e3900dd51ff5b15b10497b9d2a97
ldamus <ldamus>
2006-04-12 19:35:37 +0000

    [135652] gmf_head ldamus 060412 The Modeling Preference Page Needs To Be Removed

--- org.eclipse.gmf.runtime.emf.ui/src/org/eclipse/gmf/runtime/emf/ui/preferences/PathmapsPreferencePage.java
+++ org.eclipse.gmf.runtime.emf.ui/src/org/eclipse/gmf/runtime/emf/ui/preferences/PathmapsPreferencePage.java
@@ -1,3 +1,14 @@
+/******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    IBM Corporation - initial API and implementation 
+ ****************************************************************************/
+
 package org.eclipse.gmf.runtime.emf.ui.preferences;
 
 import java.util.Iterator;
@@ -23,7 +34,15 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.dialogs.PreferenceLinkArea;
 import org.eclipse.ui.preferences.IWorkbenchPreferenceContainer;
 
-
+/**
+ * Preference page for specifying the path variables that should be considered
+ * for modeling.
+ * <P>
+ * Path variable are created on the "Linked Resources" preference page, and
+ * selected for modeling using this page.
+ * 
+ * @author Chris McGee
+ */
 public class PathmapsPreferencePage
 	extends PreferencePage implements IWorkbenchPreferencePage {
 
