02c0ad962d226951faf5f4f06a237cfa1a753c99
mmostafa <mmostafa>
2006-07-09 22:36:55 +0000

    [147090] gmf_R1_0_maintenance mmostafa  060707 Problems in CompositeLeftRightProvider: unconnected nodes affect line routing of nodes in string

--- org.eclipse.gmf.runtime.diagram.ui.providers/src/org/eclipse/gmf/runtime/diagram/ui/providers/internal/DefaultProvider.java
+++ org.eclipse.gmf.runtime.diagram.ui.providers/src/org/eclipse/gmf/runtime/diagram/ui/providers/internal/DefaultProvider.java
@@ -616,7 +616,7 @@ public abstract class DefaultProvider
             return null;
 
         PointList routePoints = points;
-        if (source.data != connectEP.getSource()) {
+        if (source.data == connectEP.getTarget()) {
             routePoints = new PointList(points.size());
             reverse(points, routePoints);
             Node tmpNode = source;
@@ -624,11 +624,24 @@ public abstract class DefaultProvider
             target = tmpNode;
         }
         
+        int totalEdgeDiffX = diffX ;
+        int totalEdgeDiffY = diffY ;
+        Node parent=  null;
+        parent = source.getParent();
+        if (parent==null)
+            parent = target.getParent();
+        if (parent!=null){
+            Rectangle targetExt = getNodeMetrics(parent);
+            totalEdgeDiffX += targetExt.x;
+            totalEdgeDiffY += targetExt.y;
+        }
+        
+        
         PointList allPoints = new PointList(routePoints.size());
         for (int i = 0; i < routePoints.size(); i++) {
-            allPoints.addPoint(routePoints.getPoint(i).x + diffX, routePoints
+            allPoints.addPoint(routePoints.getPoint(i).x + totalEdgeDiffX, routePoints
                 .getPoint(i).y
-                + diffY);
+                + totalEdgeDiffY);
         }
 
         Rectangle sourceExt = getNodeMetrics(source);
