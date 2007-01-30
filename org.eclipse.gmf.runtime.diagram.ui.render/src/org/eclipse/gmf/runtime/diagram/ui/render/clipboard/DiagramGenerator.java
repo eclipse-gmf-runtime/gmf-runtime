e4dbc956854feeb3905c521b866d42fc569eeb9e
mmostafa <mmostafa>
2007-01-30 16:16:00 +0000

    [172052]  gmf__R1_0_maintenace mmostafa 070129 getDiagramPartInfo returns empty list (regression)

--- org.eclipse.gmf.runtime.diagram.ui.render/src/org/eclipse/gmf/runtime/diagram/ui/render/clipboard/DiagramGenerator.java
+++ org.eclipse.gmf.runtime.diagram.ui.render/src/org/eclipse/gmf/runtime/diagram/ui/render/clipboard/DiagramGenerator.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2002, 2006 IBM Corporation and others.
+ * Copyright (c) 2002, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -627,6 +627,7 @@ abstract public class DiagramGenerator {
 		Iterator li = children.iterator();
 		while (li.hasNext()) {
 			IGraphicalEditPart part = (IGraphicalEditPart) li.next();
+            editParts.add(part);
 			getNestedEditParts(part, editParts);
 		}
 
