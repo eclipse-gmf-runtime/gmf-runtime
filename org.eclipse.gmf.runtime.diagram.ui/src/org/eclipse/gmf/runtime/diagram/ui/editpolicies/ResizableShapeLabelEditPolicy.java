a8f8e6cf69de939cb92cea716393cc3056886628
crevells <crevells>
2007-01-31 16:07:12 +0000

    [172311] gmf_R1_0_maintenance crevells 070131 ResizableShapeLabelEditPolicy loses location on resize

--- org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/editpolicies/ResizableShapeLabelEditPolicy.java
+++ org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/editpolicies/ResizableShapeLabelEditPolicy.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2004, 2006 IBM Corporation and others.
+ * Copyright (c) 2004, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -167,21 +167,26 @@ public class ResizableShapeLabelEditPolicy
 	 */
 	protected Command getResizeCommand(ChangeBoundsRequest request) {
 
-		PrecisionRectangle rect = new PrecisionRectangle(
-			getInitialFeedbackBounds().getCopy());
-		getHostFigure().translateToAbsolute(rect);
-		rect.translate(request.getMoveDelta());
-		rect.resize(request.getSizeDelta());
-		getHostFigure().translateToRelative(rect);
-		View shapeView = (View) getHost().getModel();
-        
+        PrecisionRectangle rect = new PrecisionRectangle(
+            getInitialFeedbackBounds().getCopy());
+        getHostFigure().translateToAbsolute(rect);
+        rect.translate(request.getMoveDelta());
+        rect.resize(request.getSizeDelta());
+        getHostFigure().translateToRelative(rect);
+        View shapeView = (View) getHost().getModel();
+
         TransactionalEditingDomain editingDomain = ((IGraphicalEditPart) getHost())
             .getEditingDomain();
 
-		ICommand resizeCommand = new SetBoundsCommand(editingDomain,
-			DiagramUIMessages.SetLocationCommand_Label_Resize,
-			new EObjectAdapter(shapeView), rect.getSize());
-		return new ICommandProxy(resizeCommand);
+        Point refPoint = getReferencePoint();
+        Point normalPoint = LabelHelper.offsetFromRelativeCoordinate(
+            getHostFigure(), rect, refPoint);
+
+        ICommand resizeCommand = new SetBoundsCommand(editingDomain,
+            DiagramUIMessages.SetLocationCommand_Label_Resize,
+            new EObjectAdapter(shapeView), new Rectangle(normalPoint, rect
+                .getSize()));
+        return new ICommandProxy(resizeCommand);
 	}
 
 	/**
