0b15738085bf81847d6045f8c6a4282d8bb53707
mmostafa <mmostafa>
2007-02-10 00:03:15 +0000

    [173709] gmf_head mmostafa 070209 Memory leak caused by the LogicPaletteFactory

--- org.eclipse.gmf.examples.runtime.diagram.logic/src/org/eclipse/gmf/examples/runtime/diagram/logic/internal/providers/LogicPaletteFactory.java
+++ org.eclipse.gmf.examples.runtime.diagram.logic/src/org/eclipse/gmf/examples/runtime/diagram/logic/internal/providers/LogicPaletteFactory.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2004, 2006 IBM Corporation and others.
+ * Copyright (c) 2004, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,50 +11,40 @@
 
 package org.eclipse.gmf.examples.runtime.diagram.logic.internal.providers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.eclipse.gef.Tool;
 import org.eclipse.gmf.examples.runtime.diagram.logic.semantic.util.LogicSemanticType;
 import org.eclipse.gmf.runtime.diagram.ui.services.palette.PaletteFactory;
-import org.eclipse.gmf.runtime.diagram.ui.tools.ConnectionCreationTool;
 import org.eclipse.gmf.runtime.diagram.ui.tools.CreationTool;
 
 /**
  * A palette factory for Logic Entries
  * 
- * @author qili
+ * @author qili, mmostafa
  * @canBeSeenBy org.eclipse.gmf.examples.runtime.diagram.logic.*
  */
 public class LogicPaletteFactory
 	extends PaletteFactory.Adapter {
 	
-	/** list of supported tool types. */
-	private Map toolMap = new HashMap();
-	{
-		toolMap.put( LogicConstants.TOOL_LED, new CreationTool(LogicSemanticType.LED) );
-		toolMap.put( LogicConstants.TOOL_CIRCUIT, new CreationTool(LogicSemanticType.CIRCUIT) );
-		toolMap.put( LogicConstants.TOOL_ORGATE, new CreationTool(LogicSemanticType.ORGATE) );
-		toolMap.put( LogicConstants.TOOL_ANDGATE, new CreationTool(LogicSemanticType.ANDGATE) );
-		toolMap.put( LogicConstants.TOOL_XORGATE, new CreationTool(LogicSemanticType.XORGATE) );
-		toolMap.put( LogicConstants.TOOL_FLOWCONTAINER, new CreationTool(LogicSemanticType.FLOWCONTAINER) );
-	}
-	
-	/** list of supported tool types. */
-	private Map connectorMap = new HashMap();
-	{
-		connectorMap.put( LogicConstants.TOOL_CONNECTION, new ConnectionCreationTool(LogicSemanticType.WIRE) );
-	}
-
 	/*
 	 *  Create the tool according to type  	 
 	 */
 	public Tool createTool(String toolId) {
-		
-		if (toolId.equals(LogicConstants.TOOL_CONNECTION)) {
-			return (ConnectionCreationTool)connectorMap.get(toolId);
-		} else {
-			return (CreationTool)toolMap.get(toolId);
-		}
+        if (toolId.equals(LogicConstants.TOOL_LED)){
+            return new CreationTool(LogicSemanticType.LED);
+        }else if (toolId.equals(LogicConstants.TOOL_CIRCUIT)) {
+            return new CreationTool(LogicSemanticType.CIRCUIT);
+        }else if (toolId.equals(LogicConstants.TOOL_ORGATE)) {
+            return new CreationTool(LogicSemanticType.ORGATE);
+        }else if (toolId.equals(LogicConstants.TOOL_ANDGATE)) {
+            return new CreationTool(LogicSemanticType.ANDGATE);
+        }else if (toolId.equals(LogicConstants.TOOL_XORGATE)) {
+            return new CreationTool(LogicSemanticType.XORGATE);
+        }else if (toolId.equals(LogicConstants.TOOL_FLOWCONTAINER)) {
+            return new CreationTool(LogicSemanticType.FLOWCONTAINER);
+        
+        }else if (toolId.equals(LogicConstants.TOOL_CONNECTION)) {
+            return new CreationTool(LogicSemanticType.WIRE);
+        }
+        return null;
 	}
 }
