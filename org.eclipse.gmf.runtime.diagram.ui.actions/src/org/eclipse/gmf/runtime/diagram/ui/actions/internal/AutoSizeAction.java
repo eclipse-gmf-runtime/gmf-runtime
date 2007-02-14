611d1233c13751a0b4a5b7a4520d2d3fd7b8e94c
crevells <crevells>
2007-02-14 19:31:16 +0000

    [173686] gmf_head crevells 070214 Undoing debug code accidentally committed with fix for bugzilla 173686

--- org.eclipse.gmf.runtime.diagram.ui.actions/src/org/eclipse/gmf/runtime/diagram/ui/actions/internal/AutoSizeAction.java
+++ org.eclipse.gmf.runtime.diagram.ui.actions/src/org/eclipse/gmf/runtime/diagram/ui/actions/internal/AutoSizeAction.java
@@ -11,24 +11,12 @@
 
 package org.eclipse.gmf.runtime.diagram.ui.actions.internal;
 
-import java.util.Iterator;
-import java.util.List;
-
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.draw2d.geometry.Point;
-import org.eclipse.draw2d.geometry.PointList;
-import org.eclipse.gef.ConnectionEditPart;
-import org.eclipse.gef.EditPart;
 import org.eclipse.gef.Request;
-import org.eclipse.gef.commands.Command;
-import org.eclipse.gef.commands.CompoundCommand;
 import org.eclipse.gmf.runtime.diagram.ui.actions.ActionIds;
 import org.eclipse.gmf.runtime.diagram.ui.actions.DiagramAction;
 import org.eclipse.gmf.runtime.diagram.ui.actions.internal.l10n.DiagramUIActionsMessages;
 import org.eclipse.gmf.runtime.diagram.ui.actions.internal.l10n.DiagramUIActionsPluginImages;
-import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.requests.RequestConstants;
-import org.eclipse.gmf.runtime.draw2d.ui.figures.PolylineConnectionEx;
 import org.eclipse.ui.IWorkbenchPage;
 
 /**
@@ -37,63 +25,33 @@ import org.eclipse.ui.IWorkbenchPage;
  */
 public class AutoSizeAction extends DiagramAction {
 
-	/**
-	 * @param workbenchPage
-	 */
-	public AutoSizeAction(IWorkbenchPage workbenchPage) {
-		super(workbenchPage);
+    /**
+     * @param workbenchPage
+     */
+    public AutoSizeAction(IWorkbenchPage workbenchPage) {
+        super(workbenchPage);
 
-		setText(DiagramUIActionsMessages.AutoSizeAction_ActionLabelText);
-		setId(ActionIds.ACTION_AUTOSIZE);
-		setToolTipText(DiagramUIActionsMessages.AutoSizeAction_ActionToolTipText);
-		
-		setImageDescriptor(DiagramUIActionsPluginImages.DESC_AUTOSIZE);
-		setDisabledImageDescriptor(DiagramUIActionsPluginImages.DESC_AUTOSIZE_DISABLED);
-		setHoverImageDescriptor(DiagramUIActionsPluginImages.DESC_AUTOSIZE);
-	}
-
-	/**
-	 * @see org.eclipse.gmf.runtime.diagram.ui.actions.DiagramAction#createTargetRequest()
-	 */
-	protected Request createTargetRequest() {
-		return new Request(RequestConstants.REQ_AUTOSIZE);
-	}
-
-	/**
-	 * @see org.eclipse.gmf.runtime.common.ui.action.AbstractActionHandler#isSelectionListener()
-	 */
-	protected boolean isSelectionListener() {
-		return true;
-	}
-
-    protected void doRun(IProgressMonitor progressMonitor) {
-        List operationSet = getOperationSet();
-        Iterator editParts = operationSet.iterator();
-        while (editParts.hasNext()) {
-            IGraphicalEditPart ep = (IGraphicalEditPart) editParts.next();
-            System.out.println("------------------");
-            System.out.println(ep.getFigure().getBounds());
-            for (Iterator iter = ep.getSourceConnections().iterator(); iter.hasNext();) {
-                ConnectionEditPart connectionEP = (ConnectionEditPart) iter.next();
-                PointList points = ((PolylineConnectionEx)connectionEP.getFigure()).getPoints();
-                for (int i = 0; i < points.size(); i++) {
-                    Point pt = points.getPoint(i);
-                    System.out.println("  " + pt);           
-                }
-                System.out.println(((IGraphicalEditPart)connectionEP.getTarget()).getFigure().getBounds());
-            }
-            for (Iterator iter = ep.getTargetConnections().iterator(); iter.hasNext();) {
-                ConnectionEditPart connectionEP = (ConnectionEditPart) iter.next();
-                PointList points = ((PolylineConnectionEx)connectionEP.getFigure()).getPoints();
-                for (int i = 0; i < points.size(); i++) {
-                    Point pt = points.getPoint(i);
-                    System.out.println("  " + pt);           
-                }
-                System.out.println(((IGraphicalEditPart)connectionEP.getSource()).getFigure().getBounds());
-            }
-        }
+        setText(DiagramUIActionsMessages.AutoSizeAction_ActionLabelText);
+        setId(ActionIds.ACTION_AUTOSIZE);
+        setToolTipText(DiagramUIActionsMessages.AutoSizeAction_ActionToolTipText);
         
-        super.doRun(progressMonitor);
+        setImageDescriptor(DiagramUIActionsPluginImages.DESC_AUTOSIZE);
+        setDisabledImageDescriptor(DiagramUIActionsPluginImages.DESC_AUTOSIZE_DISABLED);
+        setHoverImageDescriptor(DiagramUIActionsPluginImages.DESC_AUTOSIZE);
+    }
+
+    /**
+     * @see org.eclipse.gmf.runtime.diagram.ui.actions.DiagramAction#createTargetRequest()
+     */
+    protected Request createTargetRequest() {
+        return new Request(RequestConstants.REQ_AUTOSIZE);
+    }
+
+    /**
+     * @see org.eclipse.gmf.runtime.common.ui.action.AbstractActionHandler#isSelectionListener()
+     */
+    protected boolean isSelectionListener() {
+        return true;
     }
 
 }
