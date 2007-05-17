e2c4e6d0cc72b66165d6aa156b16f668fe16dbb8
mmostafa <mmostafa>
2007-05-17 20:57:46 +0000

    Fixing Java Doc and Copyright

--- org.eclipse.gmf.runtime.diagram.ui.actions/src/org/eclipse/gmf/runtime/diagram/ui/actions/internal/SelectConnectionEndAction.java
+++ org.eclipse.gmf.runtime.diagram.ui.actions/src/org/eclipse/gmf/runtime/diagram/ui/actions/internal/SelectConnectionEndAction.java
@@ -1,3 +1,14 @@
+/******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    IBM Corporation - initial API and implementation 
+ ****************************************************************************/
+
 package org.eclipse.gmf.runtime.diagram.ui.actions.internal;
 
 import java.util.Collections;
@@ -13,23 +24,48 @@ import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.requests.SelectConnectionEndRequest;
 import org.eclipse.ui.IWorkbenchPage;
 
+
+/**
+ * @author mmostafa
+ * 
+ * Action to select one of the connection's ends
+ *
+ */
 public class SelectConnectionEndAction extends DiagramAction{
 	
 	private boolean isSource = false;
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.gmf.runtime.diagram.ui.actions.DiagramAction#createTargetRequest()
+	 */
 	protected Request createTargetRequest() {
 		return new SelectConnectionEndRequest(isSource);
 	}
 	
 	
+	/**
+     * creats a select connection source action
+	 * @param workbenchPage
+	 * @return action that will select the connection source
+	 */
 	static SelectConnectionEndAction createSelectConnectionSourceAction(IWorkbenchPage workbenchPage){
 		return new SelectConnectionEndAction(workbenchPage,true); 
 	}
 	
+    /**
+     * creats a select connection target action
+     * @param workbenchPage
+     * @return action that will select the connection target
+     */
 	static SelectConnectionEndAction createSelectConnectionTargetAction(IWorkbenchPage workbenchPage){
 		return new SelectConnectionEndAction(workbenchPage,false);
 	}
 
+	/**
+     * constructor
+	 * @param workbenchPage
+	 * @param true means select connection source, false means select connection end
+	 */
 	public SelectConnectionEndAction(IWorkbenchPage workbenchPage, boolean source) {
 	        super(workbenchPage);
 	        isSource = source;
@@ -44,11 +80,17 @@ public class SelectConnectionEndAction extends DiagramAction{
 			
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.gmf.runtime.diagram.ui.actions.DiagramAction#isSelectionListener()
+	 */
 	protected boolean isSelectionListener() {
 		return false;
 	}
 	
 	
+	/* (non-Javadoc)
+	 * @see org.eclipse.gmf.runtime.diagram.ui.actions.DiagramAction#calculateEnabled()
+	 */
 	protected boolean calculateEnabled() {
 		List operationSet = getOperationSet();
 		if (operationSet.size()!=1)
@@ -57,6 +99,9 @@ public class SelectConnectionEndAction extends DiagramAction{
 	}
 
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.gmf.runtime.diagram.ui.actions.DiagramAction#doRun(org.eclipse.core.runtime.IProgressMonitor)
+	 */
 	protected void doRun(IProgressMonitor progressMonitor) {
 		List operationSet = getOperationSet();
 		if (operationSet.isEmpty())
@@ -75,6 +120,9 @@ public class SelectConnectionEndAction extends DiagramAction{
 	}
 
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.gmf.runtime.diagram.ui.actions.DiagramAction#getTargetEditParts(org.eclipse.gef.EditPart)
+	 */
 	protected List getTargetEditParts(EditPart editpart) {
 		return Collections.singletonList(editpart);
 	}
