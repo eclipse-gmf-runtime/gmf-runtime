3c3bb08c13ce67fa9ff09eb9441fb43c33338b32
ahunter <ahunter>
2007-05-24 18:36:20 +0000

    [188962] gmf-head ahunter 070524 NPE in DiagramEditorInput

--- org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/parts/DiagramEditorInput.java
+++ org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/parts/DiagramEditorInput.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2002, 2006 IBM Corporation and others.
+ * Copyright (c) 2002, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -185,6 +185,9 @@ public class DiagramEditorInput implements IDiagramEditorInput {
 	private IFile getStorageUnitFile() {
 		try {
 			final TransactionalEditingDomain editingDomain = TransactionUtil.getEditingDomain(getDiagram());
+			if (editingDomain == null) {
+			    return null;
+			}
 			return (IFile) editingDomain.runExclusive(new RunnableWithResult.Impl() {
 
 				public void run() {
