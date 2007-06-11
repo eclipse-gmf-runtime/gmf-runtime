99510c0b506ed4f639a1f99319c2dd2f8dc8f3b4
aboyko <aboyko>
2007-06-11 20:44:58 +0000

    [192037] gmf_head aboyko 070611 SVG Image tried to save in a read-only folder must give an error message

--- org.eclipse.gmf.runtime.diagram.ui.render/src/org/eclipse/gmf/runtime/diagram/ui/render/util/CopyToImageUtil.java
+++ org.eclipse.gmf.runtime.diagram.ui.render/src/org/eclipse/gmf/runtime/diagram/ui/render/util/CopyToImageUtil.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -328,6 +328,10 @@ public class CopyToImageUtil {
         } catch (IOException ex) {
             Log.error(DiagramUIRenderPlugin.getInstance(), IStatus.ERROR, ex
                 .getMessage(), ex);
+            IStatus status =
+                new Status(IStatus.ERROR, "exportToFile", IStatus.OK, //$NON-NLS-1$
+                    ex.getMessage(), null);
+            throw new CoreException(status);
         }
     }
 
