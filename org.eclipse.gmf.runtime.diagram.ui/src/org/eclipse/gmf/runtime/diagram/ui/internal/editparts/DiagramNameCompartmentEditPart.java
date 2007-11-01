e5198b43397f82b804615a4c682e85c4eeff89dc
crevells <crevells>
2007-11-01 19:48:44 +0000

    [208220] gmf_R2_0_maintenance crevells 071101 DiagramNameCompartmentEditpart does not listen to changes to the diagram

--- org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/internal/editparts/DiagramNameCompartmentEditPart.java
+++ org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/internal/editparts/DiagramNameCompartmentEditPart.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2004, 2006 IBM Corporation and others.
+ * Copyright (c) 2004, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -71,6 +71,14 @@ public class DiagramNameCompartmentEditPart extends TextCompartmentEditPart {
         label.setTextWrap(true);
         label.setTextWrapAlignment(PositionConstants.CENTER);
         return label;
+    }
+    
+    public void activate() {
+        super.activate();
+        EObject semanticProxy = getElement();
+        if (semanticProxy != null && !semanticProxy.eIsProxy()) {
+            addSemanticListeners();
+        }
     }
 
     /** Return the semantic element associated to this editpart. */
