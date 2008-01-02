52c6310e24bcb473df80d82c9e83f1f23f2a1f85
aboyko <aboyko>
2008-01-02 22:41:56 +0000

    [214142] gmf_R2_0_maintenance aboyko 080102 [JUnit] Failure at org.eclipse.gmf.tests.runtime.diagram.ui.tools.RubberBandSelectionToolTest#test_basicSelect()

--- org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/internal/tools/RubberbandSelectionTool.java
+++ org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/internal/tools/RubberbandSelectionTool.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -203,7 +203,7 @@ private HashSet getAllChildren(EditPart editPart, HashSet allChildren1){
 private HashSet getAllChildren() {
 	if (allChildren.isEmpty())
 		allChildren = getAllChildren(
-			getCurrentViewer().getRootEditPart(),
+			getCurrentViewer().getContents(),
 			new HashSet());
 	return allChildren;
 }
