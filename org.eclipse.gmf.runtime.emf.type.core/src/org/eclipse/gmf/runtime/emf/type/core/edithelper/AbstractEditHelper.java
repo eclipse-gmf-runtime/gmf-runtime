be85cdf49717fdada4551e0579daf6eb24a9b9cd
aboyko <aboyko>
2008-01-04 20:33:12 +0000

    [214390] gmf_R2_0_maintenance aboyko 080104 [JUnit] Failure: org.eclipse.gmf.tests.runtime.emf.type.core.commands.DestroyElementCommandTest

--- org.eclipse.gmf.runtime.emf.type.core/src/org/eclipse/gmf/runtime/emf/type/core/edithelper/AbstractEditHelper.java
+++ org.eclipse.gmf.runtime.emf.type.core/src/org/eclipse/gmf/runtime/emf/type/core/edithelper/AbstractEditHelper.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -737,7 +737,11 @@ public abstract class AbstractEditHelper
 	protected ICommand getDestroyElementCommand(DestroyElementRequest req) {
 		ICommand result = null;	
 		
-		EObject parent = req.getElementToDestroy();		
+		EObject parent = req.getElementToDestroy();
+		
+		if (req.getParameter(DestroyElementRequest.INITIAL_ELEMENT_TO_DESTROY_PARAMETER) == null) {
+			req.setParameter(DestroyElementRequest.INITIAL_ELEMENT_TO_DESTROY_PARAMETER, parent);
+		}
 		
 		IElementType parentType = null;	
 		
