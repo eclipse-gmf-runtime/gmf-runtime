a75932361a859ce93fca0ab95951e5ae1428cd0f
ahunter <ahunter>
2008-01-16 21:58:55 +0000

    [191709] gmf-R2_0_maintenance ahunter 080116 AbstractModelerPropertySection should refresh if an executed command fails

--- org.eclipse.gmf.runtime.diagram.ui.properties/src/org/eclipse/gmf/runtime/diagram/ui/properties/sections/AbstractModelerPropertySection.java
+++ org.eclipse.gmf.runtime.diagram.ui.properties/src/org/eclipse/gmf/runtime/diagram/ui/properties/sections/AbstractModelerPropertySection.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2003, 2007 IBM Corporation and others.
+ * Copyright (c) 2003, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -278,8 +278,8 @@ public abstract class AbstractModelerPropertySection
                 new NullProgressMonitor(), null);
 
 			if (status.getCode() == DiagramPropertiesStatusCodes.CANCELLED
-					|| status.getCode() == IStatus.CANCEL
-					|| status.getCode() == IStatus.ERROR) {
+					|| status.getSeverity() == IStatus.CANCEL
+					|| status.getSeverity() == IStatus.ERROR) {
 				refresh();
 			}
 
