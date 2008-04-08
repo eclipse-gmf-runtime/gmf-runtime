b51d169301a09f7c816283a40e1caf27369d30f8
ahunter <ahunter>
2008-04-08 02:19:57 +0000

    gmf-head ahunter 080407 Remove TODO To change the template for this generated file go to

--- org.eclipse.gmf.runtime.diagram.ui.geoshapes/src/org/eclipse/gmf/runtime/diagram/ui/geoshapes/internal/draw2d/figures/GeoShapeFigure.java
+++ org.eclipse.gmf.runtime.diagram.ui.geoshapes/src/org/eclipse/gmf/runtime/diagram/ui/geoshapes/internal/draw2d/figures/GeoShapeFigure.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,12 +8,6 @@
  * Contributors:
  *    IBM Corporation - initial API and implementation 
  ****************************************************************************/
-/*
- * Created on Jun 22, 2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package org.eclipse.gmf.runtime.diagram.ui.geoshapes.internal.draw2d.figures;
 
 import java.util.List;
--- org.eclipse.gmf.tests.runtime.diagram.ui/src/org/eclipse/gmf/tests/runtime/diagram/ui/util/DiagramCreator.java
+++ org.eclipse.gmf.tests.runtime.diagram.ui/src/org/eclipse/gmf/tests/runtime/diagram/ui/util/DiagramCreator.java
@@ -8,12 +8,6 @@
  * Contributors:
  *    IBM Corporation - initial API and implementation 
  ****************************************************************************/
-/*
- * Created on Mar 13, 2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package org.eclipse.gmf.tests.runtime.diagram.ui.util;
 
 import java.util.ArrayList;
