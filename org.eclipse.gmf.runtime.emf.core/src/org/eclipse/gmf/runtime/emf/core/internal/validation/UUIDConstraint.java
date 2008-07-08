df9bd11304eff5e2092854ee53936599320f2375
ahunter <ahunter>
2008-07-08 19:46:20 +0000

    [238118] gmf-R2_1_maintenance laurent 080708 UUIDConstraint don't apply for resources that don't use ID or UUID

--- org.eclipse.gmf.runtime.emf.core/src/org/eclipse/gmf/runtime/emf/core/internal/validation/UUIDConstraint.java
+++ org.eclipse.gmf.runtime.emf.core/src/org/eclipse/gmf/runtime/emf/core/internal/validation/UUIDConstraint.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -64,10 +64,13 @@ public class UUIDConstraint
 		XMLResource xmlResource = (XMLResource) resource;
 		String id = xmlResource.getID(target);
 
-		EObject eObject = xmlResource.getEObject(id);
-		if (eObject != target) {
-			return ctx
-				.createFailureStatus(new Object[] {getEObjectLabel(target)});
+		// Only validate EObjects which have a UUID
+		if (null != id) {
+		   EObject eObject = xmlResource.getEObject(id);
+		   if (eObject != target) {
+		      return ctx
+				   .createFailureStatus(new Object[] {getEObjectLabel(target)});
+		   }
 		}
 
 		return ctx.createSuccessStatus();
