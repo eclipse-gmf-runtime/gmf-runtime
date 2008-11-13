43fdf3369c29b8accbc8197bca090fcebe9aeab3
ahunter <ahunter>
2008-11-13 17:12:30 +0000

    [235549] gmf-head mgobeil 081113 Set font to "Modern" cut off the text in WrappingLabel

--- org.eclipse.gmf.runtime.draw2d.ui/src/org/eclipse/gmf/runtime/draw2d/ui/text/TextUtilitiesEx.java
+++ org.eclipse.gmf.runtime.draw2d.ui/src/org/eclipse/gmf/runtime/draw2d/ui/text/TextUtilitiesEx.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,6 +16,8 @@ import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.gmf.runtime.draw2d.ui.mapmode.IMapMode;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.widgets.Shell;
 
 /**
  * @author satif, crevells
@@ -30,6 +32,11 @@ public class TextUtilitiesEx
     private IMapMode mapmode;
 
     /**
+     * Private graphics context used to measure rendered text metrics    
+     */
+    private static GC gc;
+    
+    /**
      * Creates a new instance.
      * @param mapmode mapmode to be used for translating measurement units
      */
@@ -38,46 +45,111 @@ public class TextUtilitiesEx
         this.mapmode = mapmode;
     }
 
+    /**
+     *  Gets the ascent, converted by the mapmode
+     */
     public int getAscent(Font font) {
-        int ascent = super.getAscent(font);
-        return mapmode.DPtoLP(ascent);
+        return mapmode.DPtoLP(super.getAscent(font));
     }
 
+    /**
+     *  Gets the descent, converted by the mapmode
+     */
     public int getDescent(Font font) {
-        int descent = super.getDescent(font);
-        return mapmode.DPtoLP(descent);
+        return mapmode.DPtoLP(super.getDescent(font));
     }
 
+    /**
+     *  Gets a string's extents, converted by the mapmode
+     */
     public Dimension getStringExtents(String s, Font f) {
-        Dimension dimension = super.getStringExtents(s, f);
-
-        // If the font is in italics, this does not always return the correct
-        // size and the text can get clipped. See
-        // https://bugs.eclipse.org/bugs/show_bug.cgi?id=5190. Compensate for
-        // this here until the bugzilla is fixed in SWT by adding an extra half
-        // a character space here.
-        if ((f.getFontData()[0].getStyle() & SWT.ITALIC) != 0) {
-            dimension.width += (dimension.width / s.length()) / 2;
-        }
-
-        return new Dimension(mapmode.DPtoLP(dimension.width), mapmode
-            .DPtoLP(dimension.height));
+    	Dimension extents = new Dimension(getStringDimension(s, f, true));
+        applyItalicBugHack(s, f, extents);
+        applyMapModeConversion(extents);
+        return extents;
     }
 
+    /**
+     *  Gets text's extents, converted by the mapmode
+     */
     public Dimension getTextExtents(String s, Font f) {
-        Dimension dimension = super.getTextExtents(s, f);
-
-        // If the font is in italics, this does not always return the correct
-        // size and the text can get clipped. See
-        // https://bugs.eclipse.org/bugs/show_bug.cgi?id=5190. Compensate for
-        // this here until the bugzilla is fixed in SWT by adding an extra half
-        // a character space here.
-        if ((f.getFontData()[0].getStyle() & SWT.ITALIC) != 0 && s.length() > 0) {
-            dimension.width += (dimension.width / s.length()) / 2;
-        }
-
-        return new Dimension(mapmode.DPtoLP(dimension.width), mapmode
-            .DPtoLP(dimension.height));
+        Dimension extents = new Dimension(getTextDimension(s, f, true));
+        applyItalicBugHack(s, f, extents);
+        applyMapModeConversion(extents);
+        return extents;
     }
 
+    /**
+     * Uses an offscreen GC to obtain text's rendered dimensions in pixels
+     * 
+     * @param s
+     * @param f
+     * @param advancedGraphics
+     * @return
+     */
+    private static org.eclipse.swt.graphics.Point getTextDimension(String s, Font f, boolean advancedGraphics) {
+    	return getGC(f, advancedGraphics).textExtent(s);
+    }
+
+    /**
+     * Uses an offscreen GC to obtain a string's rendered dimensions in pixels
+     * 
+     * @param s
+     * @param f
+     * @param advancedGraphics
+     * @return
+     */
+    private static org.eclipse.swt.graphics.Point getStringDimension(String s, Font f, boolean advancedGraphics) {
+    	return getGC(f, advancedGraphics).stringExtent(s);
+    }
+    
+    /**
+     * Converts a dimension using mapmode.DPtoLP()
+     * 
+     * @param extents
+     */
+    private void applyMapModeConversion(Dimension extents)
+    {
+    	extents.width = mapmode.DPtoLP(extents.width);
+    	extents.height = mapmode.DPtoLP(extents.height);
+    }
+    
+    /**
+     * If the font is in italics, this does not always return the correct
+     * size and the text can get clipped. See
+     * https://bugs.eclipse.org/bugs/show_bug.cgi?id=5190. Compensate for
+     * this here until the bugzilla is fixed in SWT by adding an extra half
+     * a character space here.
+     * 
+     * @deprecated
+     * @param s the string
+     * @param f the font
+     * @param dimension the extents to be corrected
+     */
+    private static void applyItalicBugHack(String s, Font f, Dimension extents)
+    {
+        if ((f.getFontData()[0].getStyle() & SWT.ITALIC) != 0 &&
+        		s.length() > 0) {
+        	extents.width += (extents.width / s.length()) / 2;
+        }
+    }
+    
+    /**
+     * Gets a singleton instance of a private off-screen graphics
+     * context for measuring attributes of rendered text 
+     * 
+     * @param f font setting desired
+     * @param advancedGraphics mode desired
+     * @return
+     */
+    private static GC getGC(Font f, boolean advancedGraphics)
+    {
+    	if(gc == null) {
+    		gc = new GC(new Shell());
+    	}
+    	gc.setFont(f);
+    	gc.setAdvanced(advancedGraphics);
+    	return gc;
+    }
+    
 }
