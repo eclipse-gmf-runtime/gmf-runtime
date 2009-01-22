410ff7c0bcbe75fa52ab33c50a8325576c950c04
ahunter <ahunter>
2009-01-22 19:16:55 +0000

    [262066] gmf-head ahunter 090122 Export to image dialog persists stale directories

--- org.eclipse.gmf.runtime.diagram.ui.render/src/org/eclipse/gmf/runtime/diagram/ui/render/internal/dialogs/CopyToImageDialog.java
+++ org.eclipse.gmf.runtime.diagram.ui.render/src/org/eclipse/gmf/runtime/diagram/ui/render/internal/dialogs/CopyToImageDialog.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2006, 2008 IBM Corporation and others.
+ * Copyright (c) 2006, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -711,6 +711,13 @@ public class CopyToImageDialog extends Dialog {
 	protected void okPressed() {
 		super.okPressed();
 		saveDialogSettings();
+	}
+
+	@Override
+	protected Control createButtonBar(Composite parent) {
+		Control abuttonBar = super.createButtonBar(parent);
+		validateFolderText();
+		return abuttonBar;
 	}
 
 }
