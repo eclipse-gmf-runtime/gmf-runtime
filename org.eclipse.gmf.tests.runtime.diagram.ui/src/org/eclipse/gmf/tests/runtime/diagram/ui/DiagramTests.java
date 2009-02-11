abd50ceac4fa94760af4ec2245c2b4dbe909d5ec
aboyko <aboyko>
2009-02-11 01:56:56 +0000

    [264438] gmf_head aboyko 090210 Diagram#setMeasurementUnit should work - merged 3 junits in 2, copyrights, non-translatable strings)

--- org.eclipse.gmf.tests.runtime.diagram.ui/src/org/eclipse/gmf/tests/runtime/diagram/ui/DiagramTests.java
+++ org.eclipse.gmf.tests.runtime.diagram.ui/src/org/eclipse/gmf/tests/runtime/diagram/ui/DiagramTests.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -80,23 +80,26 @@ public class DiagramTests extends AbstractDiagramTests {
 		Diagram diagram = NotationFactory.eINSTANCE.createDiagram();
 		diagram.setMeasurementUnit(MeasurementUnit.PIXEL_LITERAL);
 		assertEquals("get just after set (pixels)", MeasurementUnit.PIXEL_LITERAL, diagram.getMeasurementUnit()); //$NON-NLS-N$
+		try {
+			diagram.setMeasurementUnit(MeasurementUnit.HIMETRIC_LITERAL);
+		} catch (UnsupportedOperationException e) {
+			
+		} finally {
+			assertEquals("MeasurementUnit can't change now", MeasurementUnit.PIXEL_LITERAL, diagram.getMeasurementUnit()); //$NON-NLS-1$		
+		}
 	}
 
-	public void testSetHiMetricMU(){
+	public void testSetHiMetricMeasurementUnit(){
 		Diagram diagram = NotationFactory.eINSTANCE.createDiagram();
 		diagram.setMeasurementUnit(MeasurementUnit.HIMETRIC_LITERAL);
 		assertEquals("get just after set (hi-metric)", MeasurementUnit.HIMETRIC_LITERAL, diagram.getMeasurementUnit()); //$NON-NLS-N$
-	}
-	
-	public void testMeasuremeantUnitSetOnceOnly() {
-		Diagram diagram = NotationFactory.eINSTANCE.createDiagram();
-		diagram.setMeasurementUnit(MeasurementUnit.HIMETRIC_LITERAL);
 		try {
 			diagram.setMeasurementUnit(MeasurementUnit.PIXEL_LITERAL);
 		} catch (UnsupportedOperationException e) {
 			
+		} finally {
+			assertEquals("MeasurementUnit can't change now", MeasurementUnit.HIMETRIC_LITERAL, diagram.getMeasurementUnit()); //$NON-NLS-1$		
 		}
-		assertEquals("MeasurementUnit can't change now", MeasurementUnit.HIMETRIC_LITERAL, diagram.getMeasurementUnit());
 	}
-
+	
 }
