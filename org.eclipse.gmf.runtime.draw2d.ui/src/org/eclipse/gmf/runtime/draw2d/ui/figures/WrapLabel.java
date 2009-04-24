1a6173461282dc5712547e724a29ec5c2864d1dd
ahunter <ahunter>
2009-04-24 18:17:49 +0000

    [272658] gmf-head mariot.chauvin 090424 NPE when calling WrapLabel#getIconBounds() if icon location has not been set

--- org.eclipse.gmf.runtime.draw2d.ui/src/org/eclipse/gmf/runtime/draw2d/ui/figures/WrapLabel.java
+++ org.eclipse.gmf.runtime.draw2d.ui/src/org/eclipse/gmf/runtime/draw2d/ui/figures/WrapLabel.java
@@ -1,12 +1,13 @@
 /******************************************************************************
- * Copyright (c) 2002, 2007 IBM Corporation and others.
+ * Copyright (c) 2002, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    IBM Corporation - initial API and implementation 
+ *    IBM Corporation - initial API and implementation
+ *    Mariot Chauvin <mariot.chauvin@obeo.fr> - bug 272658
  ****************************************************************************/
 
 package org.eclipse.gmf.runtime.draw2d.ui.figures;
@@ -156,8 +157,9 @@ public class WrapLabel
      */
     public Rectangle getIconBounds() {
         if (hasIcons()) {
+        	final Point safeIconLocation = getIconLocation() != null ? getIconLocation() : new Point(0,0);
             return new Rectangle(getBounds().getLocation().translate(
-                getIconLocation()), getTotalIconSize());
+                safeIconLocation), getTotalIconSize());
         }
         return new Rectangle(0, 0, 0, 0);
     }
