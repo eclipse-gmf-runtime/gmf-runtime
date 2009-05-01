3dd27f340517d72afdfdba2ce5b2abb289c560d1
ahunter <ahunter>
2009-05-01 21:48:51 +0000

    [273891] gmf-head ahunter 090501 [api] Cannot access property descriptor of a property sheet entry

--- org.eclipse.gmf.runtime.emf.ui.properties/src/org/eclipse/gmf/runtime/emf/ui/properties/sections/PropertySheetEntry.java
+++ org.eclipse.gmf.runtime.emf.ui.properties/src/org/eclipse/gmf/runtime/emf/ui/properties/sections/PropertySheetEntry.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2004, 2006 IBM Corporation and others.
+ * Copyright (c) 2004, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -345,7 +345,7 @@ public class PropertySheetEntry implements IPropertySheetEntry,
      * 
      * @return <code>IPropertyDescriptor</code> for this entry
      */
-    protected IPropertyDescriptor getDescriptor() {
+    public IPropertyDescriptor getDescriptor() {
         return descriptor;
     }
 
