99fd5908b1cbc3ca1ec85f13287754ef4b38c044
aboyko <aboyko>
2009-05-20 14:44:52 +0000

    [276511] gmf_head aboyko 090520 Infinite loop caused by rounding error in RelativeToBorderLocator

--- org.eclipse.gmf.runtime.gef.ui/src/org/eclipse/gmf/runtime/gef/ui/internal/figures/RelativeToBorderLocator.java
+++ org.eclipse.gmf.runtime.gef.ui/src/org/eclipse/gmf/runtime/gef/ui/internal/figures/RelativeToBorderLocator.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2002, 2006 IBM Corporation and others.
+ * Copyright (c) 2002, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,6 +15,7 @@ import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Locator;
 import org.eclipse.draw2d.PositionConstants;
 import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.PrecisionRectangle;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.handles.HandleBounds;
 
@@ -81,8 +82,8 @@ public class RelativeToBorderLocator implements Locator {
 	public void relocate(IFigure target) {
 		Rectangle bounds =
 			reference instanceof HandleBounds
-				? ((HandleBounds) reference).getHandleBounds().getCopy()
-				: reference.getBounds().getCopy();	
+				? new PrecisionRectangle(((HandleBounds) reference).getHandleBounds())
+				: new PrecisionRectangle(reference.getBounds());	
 
 		reference.translateToAbsolute(bounds);
 		target.translateToRelative(bounds);
