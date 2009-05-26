8dd9206ad4afb28a5e0528e6b16a80d6abf54c48
aboyko <aboyko>
2009-05-26 20:14:03 +0000

    [272529] gmf_head aboyko 090526 NPE in BaseSlidableAnchor.hashCode

--- org.eclipse.gmf.runtime.draw2d.ui/src/org/eclipse/gmf/runtime/draw2d/ui/figures/BaseSlidableAnchor.java
+++ org.eclipse.gmf.runtime.draw2d.ui/src/org/eclipse/gmf/runtime/draw2d/ui/figures/BaseSlidableAnchor.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2006, 2008 IBM Corporation and others.
+ * Copyright (c) 2006, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -131,7 +131,7 @@ public class BaseSlidableAnchor
 	 */
 	public int hashCode() {
 		int figureHashCode = getOwner() != null ? getOwner().hashCode() : 0; 
-		if (isDefaultAnchor()) {
+		if (relativeReference == null) {
 			return figureHashCode;
 		}
 		return new Double(relativeReference.preciseX()).hashCode()
