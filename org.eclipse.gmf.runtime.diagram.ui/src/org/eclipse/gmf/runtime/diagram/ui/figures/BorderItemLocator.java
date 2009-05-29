2ab4398f6a366f967fb8e7ef18fe3f8c415a45af
aboyko <aboyko>
2009-05-29 15:38:12 +0000

    [275288] gmf_R2_1_maintencance aboyko 090529 Application freezes in BorderItemLocator.locateOnBorder

--- org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/figures/BorderItemLocator.java
+++ org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/figures/BorderItemLocator.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -432,11 +432,11 @@ public class BorderItemLocator
 			break;
 		case PositionConstants.EAST:
 			currentLocation.y = obstacle.getTopRight().y - interval
-					- borderItem.getBounds().height;
+					- getSize(borderItem).height;
 			break;
 		case PositionConstants.NORTH: // Move towards the west side
 			currentLocation.x = obstacle.getTopLeft().x - interval
-					- borderItem.getBounds().width;
+					- getSize(borderItem).width;
 			break;
 		default:
 			throw new IllegalArgumentException(
