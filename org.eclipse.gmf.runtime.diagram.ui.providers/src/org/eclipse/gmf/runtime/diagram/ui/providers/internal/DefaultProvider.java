2348f9061be60aa21fcfb293be894d7f826ecee6
mchauvin <mchauvin>
2009-07-03 14:50:21 +0000

    bug #282380

--- org.eclipse.gmf.runtime.diagram.ui.providers/src/org/eclipse/gmf/runtime/diagram/ui/providers/internal/DefaultProvider.java
+++ org.eclipse.gmf.runtime.diagram.ui.providers/src/org/eclipse/gmf/runtime/diagram/ui/providers/internal/DefaultProvider.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2002, 2008 IBM Corporation and others.
+ * Copyright (c) 2002, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -594,9 +594,11 @@ public abstract class DefaultProvider
                 // the associated connectionView should be included on graph
                 if (target instanceof IBorderItemEditPart)
                     target = target.getParent();
-                Object o = editPartToNodeDict.get(target);
-                if (o != null) {
-                    connectionsToMove.add(connectionEP);
+                if (target != null) {
+                	Object o = editPartToNodeDict.get(target);
+                    if (o != null) {
+                        connectionsToMove.add(connectionEP);
+                    }
                 }
             }
             
