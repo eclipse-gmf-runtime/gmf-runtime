43455aeecd45f18ade1b86fb482bd2200c8a8e61
lgrahek <lgrahek>
2009-09-11 13:47:54 +0000

    [288834] gmf_head lgrahek 090911 Need to extend ElementSelectionScope

--- org.eclipse.gmf.runtime.common.ui.services/src/org/eclipse/gmf/runtime/common/ui/services/elementselection/ElementSelectionScope.java
+++ org.eclipse.gmf.runtime.common.ui.services/src/org/eclipse/gmf/runtime/common/ui/services/elementselection/ElementSelectionScope.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -35,7 +35,7 @@ public class ElementSelectionScope {
      * @param scope
      *            the scope
      */
-    private ElementSelectionScope(int scope) {
+    protected ElementSelectionScope(int scope) {
         this.scope = scope;
     }
 
