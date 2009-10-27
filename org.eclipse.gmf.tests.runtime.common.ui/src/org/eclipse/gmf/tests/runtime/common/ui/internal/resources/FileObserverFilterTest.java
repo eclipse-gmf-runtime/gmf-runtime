1c2ede3340069fcf127fd49ffc91e0b9b756db7e
ahunter <ahunter>
2009-10-27 18:39:51 +0000

    [212422] gmf-head ahunter 091027 The type FileXXXTest.File must implement the inherited abstract method IResource.XXX()

--- org.eclipse.gmf.tests.runtime.common.ui/src/org/eclipse/gmf/tests/runtime/common/ui/internal/resources/FileObserverFilterTest.java
+++ org.eclipse.gmf.tests.runtime.common.ui/src/org/eclipse/gmf/tests/runtime/common/ui/internal/resources/FileObserverFilterTest.java
@@ -736,7 +736,6 @@ public class FileObserverFilterTest
 		/**
 		 * @see org.eclipse.core.runtime.IAdaptable#getAdapter(java.lang.Class)
 		 */
-		@SuppressWarnings("unchecked")
 		public Object getAdapter(Class adapter) {
 			return null;
 		}
