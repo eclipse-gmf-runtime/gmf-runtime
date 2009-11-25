d9fa2a72ff2e85169833974be97ea0e72267a883
ahunter <ahunter>
2009-11-25 21:35:34 +0000

    [296190] gmf-head ahunter 091125 IllegalArgumentException when closing a diagram

--- org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/parts/DiagramEditor.java
+++ org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/parts/DiagramEditor.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2002, 2008 IBM Corporation and others.
+ * Copyright (c) 2002, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -928,7 +928,9 @@ public abstract class DiagramEditor
      */
     protected void setInput(IEditorInput input) {
         stopListening();
-        super.setInput(input);
+        if (input != null) {
+        	super.setInput(input);
+        }
         if (input != null) {
             Assert.isNotNull(getDiagram(), "Couldn't load/create diagram view"); //$NON-NLS-1$
         }
