9af68366824fcb49a01af21effe0a91c55d0ab1a
aboyko <aboyko>
2010-01-04 23:00:36 +0000

    [298791] gmf_R2_2_maintenance aboyko 100104  [Layout] FreeformLayoutEx corrupts constraints

--- org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/layout/FreeFormLayoutEx.java
+++ org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/layout/FreeFormLayoutEx.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2006, 2007 IBM Corporation and others.
+ * Copyright (c) 2006, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -35,7 +35,7 @@ public class FreeFormLayoutEx
         IFigure f;
         while (children.hasNext()) {
             f = (IFigure)children.next();
-            Rectangle bounds = (Rectangle)getConstraint(f);
+            Rectangle bounds = ((Rectangle)getConstraint(f)).getCopy();
             if (bounds == null) continue;
 
             int widthHint = bounds.width;
