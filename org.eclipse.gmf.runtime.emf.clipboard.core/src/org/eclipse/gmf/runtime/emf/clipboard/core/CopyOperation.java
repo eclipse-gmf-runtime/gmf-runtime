3e5f28ecb565f5033a5aeb3e2041d5dbfad14cbf
ahunter <ahunter>
2010-01-11 20:17:10 +0000

    [297753] gmf_R2_2_maintenance ahunter 100111 Add @since flagged by API Tooling

--- org.eclipse.gmf.runtime.emf.clipboard.core/src/org/eclipse/gmf/runtime/emf/clipboard/core/CopyOperation.java
+++ org.eclipse.gmf.runtime.emf.clipboard.core/src/org/eclipse/gmf/runtime/emf/clipboard/core/CopyOperation.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2002, 2009 IBM Corporation and others.
+ * Copyright (c) 2002, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -437,6 +437,9 @@ public class CopyOperation
 			new IllegalArgumentException("Uncopyable Object")); //$NON-NLS-1$
 	}
 
+	/**
+	 * @since 1.3
+	 */
 	protected CopyParentsCopier getCopyParentsCopier() {
 		if (copyParentsCopierCopier == null) {
 			copyParentsCopierCopier = new CopyParentsCopier();
@@ -445,6 +448,9 @@ public class CopyOperation
 
 	}
 
+	/**
+	 * @since 1.3
+	 */
 	protected class CopyParentsCopier
 		extends EcoreUtil.Copier {
 
