e828d3a3a25f6acf1dc4b8a98938351e4eb7baea
ahunter <ahunter>
2010-01-21 14:38:58 +0000

    [109847] gmf-head ahunter 100121 Remove all the @canBeSeenBy comments

--- org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/editparts/ShapeNodeEditPart.java
+++ org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/editparts/ShapeNodeEditPart.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2002, 2009 IBM Corporation and others.
+ * Copyright (c) 2002, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -44,13 +44,9 @@ import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.gmf.runtime.notation.datatype.GradientData;
 import org.eclipse.swt.graphics.Color;
 
-/*
- * @canBeSeenBy %partners
- */
 /**
- * the shape node controller
+ * The shape node controller
  * @author mmostafa
- *
  */
 public abstract class ShapeNodeEditPart
 	extends ShapeEditPart
