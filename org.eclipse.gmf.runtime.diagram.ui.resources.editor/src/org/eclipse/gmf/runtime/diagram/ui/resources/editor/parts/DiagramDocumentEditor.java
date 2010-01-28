6e917f153d622698d16ae84291db5353c005bc8a
lgrahek <lgrahek>
2010-01-28 19:43:24 +0000

    [300821] gmf-head lgrahek 100128 DiagramDocumentEditor should not call setInput on dispose

--- org.eclipse.gmf.runtime.diagram.ui.resources.editor/src/org/eclipse/gmf/runtime/diagram/ui/resources/editor/parts/DiagramDocumentEditor.java
+++ org.eclipse.gmf.runtime.diagram.ui.resources.editor/src/org/eclipse/gmf/runtime/diagram/ui/resources/editor/parts/DiagramDocumentEditor.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2005, 2009 IBM Corporation and others.
+ * Copyright (c) 2005, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -207,8 +207,6 @@ public class DiagramDocumentEditor
         IStatus status = provider.getStatus(getEditorInput());
 
 		disposeDocumentProvider();
-
-        super.setInput(null);
 		
         if(status != null && status.isOK())
             super.dispose();
