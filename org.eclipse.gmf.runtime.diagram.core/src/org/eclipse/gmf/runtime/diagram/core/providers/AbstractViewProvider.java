368c467da439dc9ad7a7fec637ef045077b2b30a
ahunter <ahunter>
2010-02-16 21:33:04 +0000

    [302998] gmf-head ahunter 100216 Compiler warning in AbstractViewProvider

--- org.eclipse.gmf.runtime.diagram.core/src/org/eclipse/gmf/runtime/diagram/core/providers/AbstractViewProvider.java
+++ org.eclipse.gmf.runtime.diagram.core/src/org/eclipse/gmf/runtime/diagram/core/providers/AbstractViewProvider.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2002, 2006 IBM Corporation and others.
+ * Copyright (c) 2002, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -236,7 +236,7 @@ public class AbstractViewProvider
 				return null;
 			}
 
-			Object factory = constructor.newInstance(null);
+			Object factory = constructor.newInstance((Object)null);
 			Method method = getCreationMethod(viewClass, constructorParams);
 			if (method == null) {
 				Log
