3e6f990b68faef8d2d8e015f579f6a04d5b31cde
ahunter <ahunter>
2010-02-17 14:44:25 +0000

    [162932] gmf_head ahunter 100217 [NoteTextLabel] WrapLabel should use Gef's text layout (clean warning: remove unneeded @SuppressWarnings tag)

--- org.eclipse.gmf.runtime.draw2d.ui/src/org/eclipse/gmf/runtime/draw2d/ui/text/TextFlowEx.java
+++ org.eclipse.gmf.runtime.draw2d.ui/src/org/eclipse/gmf/runtime/draw2d/ui/text/TextFlowEx.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -45,7 +45,6 @@ public class TextFlowEx
      * 
      * @see Figure#MAX_FLAG
      */
-    @SuppressWarnings("hiding")
     protected static final int MAX_FLAG = FLAG_STRIKEDTHROUGH;
 
     private String truncationString = "..."; //$NON-NLS-1$
