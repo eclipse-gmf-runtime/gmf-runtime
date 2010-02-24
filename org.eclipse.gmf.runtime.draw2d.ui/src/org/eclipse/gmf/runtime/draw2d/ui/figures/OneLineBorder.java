fff6815d81faddb9106005373163b49de96ff7ef
ahunter <ahunter>
2010-02-24 16:33:51 +0000

    [278951] gmf-head ahunter 100224 OneLineBorder ignores color

--- org.eclipse.gmf.runtime.draw2d.ui/src/org/eclipse/gmf/runtime/draw2d/ui/figures/OneLineBorder.java
+++ org.eclipse.gmf.runtime.draw2d.ui/src/org/eclipse/gmf/runtime/draw2d/ui/figures/OneLineBorder.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2002, 2009 IBM Corporation and others.
+ * Copyright (c) 2002, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -123,6 +123,9 @@ public class OneLineBorder extends LineBorder {
 		
 		graphics.setLineWidth(getWidth());
 		graphics.setLineStyle(getStyle());
+		if (getColor() != null) {
+			graphics.setForegroundColor(getColor());
+		}
 		
 		switch (position) {
 			case PositionConstants.TOP :
