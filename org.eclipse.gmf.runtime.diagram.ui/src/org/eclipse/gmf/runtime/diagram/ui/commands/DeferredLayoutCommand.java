f25bfeceddc23492d139288be4f471af1066f950
aboyko <aboyko>
2010-03-02 19:52:15 +0000

    [303800] gmf_head aboyko 100302 ScrollBars in the logic diagram example

--- org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/commands/DeferredLayoutCommand.java
+++ org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/commands/DeferredLayoutCommand.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2002, 2009 IBM Corporation and others.
+ * Copyright (c) 2002, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -256,5 +256,11 @@ public class DeferredLayoutCommand
 	 */
 	protected List getViewAdapters() {
 		return viewAdapters;
+	}
+
+	@Override
+	public boolean canExecute() {
+		return super.canExecute() && containerEP != null
+				&& viewAdapters != null && !viewAdapters.isEmpty();
 	}
 }
