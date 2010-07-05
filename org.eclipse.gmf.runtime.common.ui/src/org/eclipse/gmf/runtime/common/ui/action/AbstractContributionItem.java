fa301a36665c240d95d5003b8ad290a8dd8e8cfd
mchauvin <mchauvin>
2010-07-05 16:58:42 +0000

    [300451] gmf_R2_2_maintenance mchauvin 100705 [Actions] Deadlock occurs when flushing redo stack on diagram during a modal action

--- org.eclipse.gmf.runtime.common.ui/src/org/eclipse/gmf/runtime/common/ui/action/AbstractContributionItem.java
+++ org.eclipse.gmf.runtime.common.ui/src/org/eclipse/gmf/runtime/common/ui/action/AbstractContributionItem.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2002, 2009 IBM Corporation and others.
+ * Copyright (c) 2002, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -655,7 +655,7 @@ public abstract class AbstractContributionItem
 	
 	            if (partContext != null && operation.hasContext(partContext)
 	            		&& PlatformUI.isWorkbenchRunning()) {
-	                PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+	                PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
 	
 	                    public void run() {
 	                        update();
