e1d39fa5505b08f2f184d83af792dc013e1b4f23
lgrahek <lgrahek>
2010-10-14 16:49:14 +0000

    [326563] gmf-head lgrahek 101014 Allow access to elementType in ConnectionCreationTool

--- org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/tools/ConnectionCreationTool.java
+++ org.eclipse.gmf.runtime.diagram.ui/src/org/eclipse/gmf/runtime/diagram/ui/tools/ConnectionCreationTool.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2002, 2007 IBM Corporation and others.
+ * Copyright (c) 2002, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -104,8 +104,9 @@ public class ConnectionCreationTool
 
 	/**
 	 * @return Returns the elementType.
+	 * @since 1.5
 	 */
-	protected IElementType getElementType() {
+	public IElementType getElementType() {
 		return elementType;
 	}
 
