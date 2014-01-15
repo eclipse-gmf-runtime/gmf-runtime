6f2f6f45ad3bc2b9bf9d73d81140b578d61b831d
Anthony Hunter <anthonyh@ca.ibm.com>
2014-01-15 15:20:55 -0500

    Bug 416891 AWT images garble japanese string Contributed by Vikas.Chandra

--- org.eclipse.gmf.runtime.draw2d.ui.render.awt/src/org/eclipse/gmf/runtime/draw2d/ui/render/awt/internal/graphics/GraphicsToGraphics2DAdaptor.java
+++ org.eclipse.gmf.runtime.draw2d.ui.render.awt/src/org/eclipse/gmf/runtime/draw2d/ui/render/awt/internal/graphics/GraphicsToGraphics2DAdaptor.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2004, 2012 IBM Corporation and others.
+ * Copyright (c) 2004, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -776,7 +776,8 @@ public class GraphicsToGraphics2DAdaptor extends Graphics implements DrawableRen
 		float ypos = y + transY;
 		int lineWidth;
 
-		if (paintNotCompatibleStringsAsBitmaps && Math.abs(swtStringSize.width - stringLength) > 2) {
+		if (paintNotCompatibleStringsAsBitmaps && 
+			( (getGraphics2D().getFont().canDisplayUpTo(s) != -1) || ( Math.abs(swtStringSize.width - stringLength) > 2) ) ) {
 			// create SWT bitmap of the string then
 			Image image = new Image(DisplayUtils.getDisplay(),
 					swtStringSize.width, swtStringSize.height);
