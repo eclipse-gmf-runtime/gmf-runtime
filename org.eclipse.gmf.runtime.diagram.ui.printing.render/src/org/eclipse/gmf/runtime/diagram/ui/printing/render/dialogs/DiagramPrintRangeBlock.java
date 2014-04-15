680bb0b3943108c1f57fc7ed1a18983b7f9f670d
Anthony Hunter <anthonyh@ca.ibm.com>
2014-04-15 10:38:28 -0400

    Bug 422445 OK button not accessible in "print diagram" dialog window

--- org.eclipse.gmf.runtime.diagram.ui.printing.render/src/org/eclipse/gmf/runtime/diagram/ui/printing/render/dialogs/DiagramPrintRangeBlock.java
+++ org.eclipse.gmf.runtime.diagram.ui.printing.render/src/org/eclipse/gmf/runtime/diagram/ui/printing/render/dialogs/DiagramPrintRangeBlock.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2008 IBM Corporation and others.
+ * Copyright (c) 2008, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -28,6 +28,7 @@ import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -82,6 +83,9 @@ public class DiagramPrintRangeBlock extends DialogBlock {
 		diagramList = list(result);
 				
 		layoutFillHorizontal(layoutHeight(diagramList.getControl(),48));
+		GridData data = getLayoutData(diagramList.getControl());
+		data.widthHint = 300;
+		
 		diagramList.getControl().setEnabled(options.isDiagramSelection());
 
 		currentDiagramRadio.addSelectionListener(new SelectionListener() {
