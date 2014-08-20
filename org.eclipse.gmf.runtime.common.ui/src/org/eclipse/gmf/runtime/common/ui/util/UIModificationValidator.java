beafa40b3e1b6a66a066a7790f4f16bb35ff75e9
Anthony Hunter <anthonyh@ca.ibm.com>
2014-08-20 13:09:47 -0400

    Bug 424074 CommonUIPlugin.stop() fails in Luna due to disposed Display

--- org.eclipse.gmf.runtime.common.ui/src/org/eclipse/gmf/runtime/common/ui/util/UIModificationValidator.java
+++ org.eclipse.gmf.runtime.common.ui/src/org/eclipse/gmf/runtime/common/ui/util/UIModificationValidator.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2005, 2009 IBM Corporation and others.
+ * Copyright (c) 2005, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -204,7 +204,7 @@ public class UIModificationValidator
 	 * Disposes this UI modification validator.
 	 */
     public void dispose() {
-        if (listener != null) {
+    	if (listener != null && !listener.getShell().isDisposed()) {
         	DisplayUtils.getDisplay().asyncExec(new Runnable() {
                 public void run() {
                     PlatformUI.getWorkbench().removeWindowListener(listener);
