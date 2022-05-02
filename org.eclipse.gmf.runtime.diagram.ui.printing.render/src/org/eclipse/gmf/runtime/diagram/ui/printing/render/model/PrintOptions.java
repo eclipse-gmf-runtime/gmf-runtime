689fea55bdb82da743e6799139238de782ebd16b
Pierre-Charles David <pierre-charles.david@obeo.fr>
2022-05-02 21:57:37 +0200

    [1] Remove usage of deprecated Observable classes
    Bug: https://github.com/eclipse/gmf-runtime/issues/1
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- org.eclipse.gmf.runtime.diagram.ui.printing.render/src/org/eclipse/gmf/runtime/diagram/ui/printing/render/model/PrintOptions.java
+++ org.eclipse.gmf.runtime.diagram.ui.printing.render/src/org/eclipse/gmf/runtime/diagram/ui/printing/render/model/PrintOptions.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2008 IBM Corporation and others.
+ * Copyright (c) 2008, 2022 IBM Corporation and others.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
