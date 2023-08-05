acc7981b9340b95c0f7cebaa035f3183aed33bf3
Pierre-Charles David <pierre-charles.david@obeo.fr>
2023-08-05 17:06:55 +0200

    [cleanup] Fix compilation warnings in IDETransferAdapterProvider
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- bundles/org.eclipse.gmf.runtime.common.ui.services.dnd.ide/src/org/eclipse/gmf/runtime/common/ui/services/dnd/ide/internal/core/IDETransferAdapterProvider.java
+++ bundles/org.eclipse.gmf.runtime.common.ui.services.dnd.ide/src/org/eclipse/gmf/runtime/common/ui/services/dnd/ide/internal/core/IDETransferAdapterProvider.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2023 IBM Corporation and others.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -7,12 +7,13 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *    IBM Corporation - initial API and implementation 
+ *    IBM Corporation - initial API and implementation
  ****************************************************************************/
 
 package org.eclipse.gmf.runtime.common.ui.services.dnd.ide.internal.core;
 
-import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.Objects;
 
 import org.eclipse.gmf.runtime.common.ui.services.dnd.core.AbstractTransferAdapterProvider;
 import org.eclipse.gmf.runtime.common.ui.services.dnd.core.TransferAgent;
@@ -31,147 +32,83 @@ import org.eclipse.ui.part.ResourceTransfer;
 
 /**
  * Concrete implementation for common transfer adapters
- * 
+ *
  * @author Wayne Diu, wdiu, based on
- * org.eclipse.gmf.runtime.common.ui.services.dnd.core.TransferAdapterProvider
+ *         org.eclipse.gmf.runtime.common.ui.services.dnd.core.TransferAdapterProvider
  */
-public final class IDETransferAdapterProvider
-	extends AbstractTransferAdapterProvider {
+public final class IDETransferAdapterProvider extends AbstractTransferAdapterProvider {
 
 	/**
 	 * Attribute for the table of transfer ids and their drag adapters
 	 */
-	private Hashtable transferDragSourceTable = new Hashtable();
+	private HashMap<Integer, ITransferDragSourceListener> transferDragSourceTable = new HashMap<>();
 
 	/**
 	 * Attribute for the table of transfer ids and their drop adapters
 	 */
-	private Hashtable transferDropTargetTable = new Hashtable();
+	private HashMap<Integer, ITransferDropTargetListener> transferDropTargetTable = new HashMap<>();
 
 	/**
 	 * Constructor for TransferAdapterProvider.
 	 */
 	public IDETransferAdapterProvider() {
-		super();
 
 		/*
-		 * Initialize the table of transfer Ids and their drag adapters for
-		 * pre-defined ids
+		 * Initialize the table of transfer Ids and their drag adapters for pre-defined
+		 * ids
 		 */
-		getTransferDragSourceTable().put(
-				Integer.valueOf(IDETransferId.RESOURCE_TRANSFER.hashCode()),
-			new TransferDragSourceAdapter(new TransferAgent(
-				IDETransferId.RESOURCE_TRANSFER, ResourceTransfer.getInstance(),
-				false)));
+		transferDragSourceTable.put(Integer.valueOf(IDETransferId.RESOURCE_TRANSFER.hashCode()),
+				new TransferDragSourceAdapter(
+						new TransferAgent(IDETransferId.RESOURCE_TRANSFER, ResourceTransfer.getInstance(), false)));
 
-		getTransferDragSourceTable().put(
-				Integer.valueOf(IDETransferId.MARKER_TRANSFER.hashCode()),
-			new TransferDragSourceAdapter(
-				new TransferAgent(IDETransferId.MARKER_TRANSFER, MarkerTransfer
-					.getInstance(), false)));
-		getTransferDragSourceTable().put(
-				Integer.valueOf(IDETransferId.NAV_SELECTION_TRANSFER.hashCode()),
-			new SelectionDragAdapter(new TransferAgent(
-				IDETransferId.NAV_SELECTION_TRANSFER, LocalSelectionTransfer
-					.getTransfer(), true) {
+		transferDragSourceTable.put(Integer.valueOf(IDETransferId.MARKER_TRANSFER.hashCode()),
+				new TransferDragSourceAdapter(
+						new TransferAgent(IDETransferId.MARKER_TRANSFER, MarkerTransfer.getInstance(), false)));
+		transferDragSourceTable.put(Integer.valueOf(IDETransferId.NAV_SELECTION_TRANSFER.hashCode()),
+				new SelectionDragAdapter(new TransferAgent(IDETransferId.NAV_SELECTION_TRANSFER,
+						LocalSelectionTransfer.getTransfer(), true) {
 
-				/*
-				 * (non-Javadoc)
-				 * 
-				 * @see org.eclipse.gmf.runtime.common.ui.services.dnd.core.ITransferAgent#getSelection(org.eclipse.swt.dnd.TransferData)
-				 */
-				public ISelection getSelection(TransferData transferData) {
-					return LocalSelectionTransfer.getTransfer().getSelection();
-				}
+					@Override
+					public ISelection getSelection(TransferData transferData) {
+						return LocalSelectionTransfer.getTransfer().getSelection();
+					}
 
-				/*
-				 * (non-Javadoc)
-				 * 
-				 * @see org.eclipse.gmf.runtime.common.ui.services.dnd.core.ITransferAgent#setSelection(org.eclipse.jface.viewers.ISelection)
-				 */
-				public void setSelection(ISelection selection) {
-					LocalSelectionTransfer.getTransfer()
-						.setSelection(selection);
-				}
-			}));
+					@Override
+					public void setSelection(ISelection selection) {
+						LocalSelectionTransfer.getTransfer().setSelection(selection);
+					}
+				}));
 		/*
-		 * Initialize the table of transfer Ids and their drop adapters for
-		 * pre-defined ids
+		 * Initialize the table of transfer Ids and their drop adapters for pre-defined
+		 * ids
 		 */
-		getTransferDropTargetTable().put(
-				Integer.valueOf(IDETransferId.RESOURCE_TRANSFER.hashCode()),
-			new TransferDropTargetAdapter(new TransferAgent(
-				IDETransferId.RESOURCE_TRANSFER, ResourceTransfer.getInstance(),
-				false)));
+		transferDropTargetTable.put(Integer.valueOf(IDETransferId.RESOURCE_TRANSFER.hashCode()),
+				new TransferDropTargetAdapter(
+						new TransferAgent(IDETransferId.RESOURCE_TRANSFER, ResourceTransfer.getInstance(), false)));
 
-		getTransferDropTargetTable().put(
-				Integer.valueOf(IDETransferId.MARKER_TRANSFER.hashCode()),
-			new TransferDropTargetAdapter(
-				new TransferAgent(IDETransferId.MARKER_TRANSFER, MarkerTransfer
-					.getInstance(), false)));
+		transferDropTargetTable.put(Integer.valueOf(IDETransferId.MARKER_TRANSFER.hashCode()),
+				new TransferDropTargetAdapter(
+						new TransferAgent(IDETransferId.MARKER_TRANSFER, MarkerTransfer.getInstance(), false)));
 
-		getTransferDropTargetTable().put(
-				Integer.valueOf(IDETransferId.NAV_SELECTION_TRANSFER.hashCode()),
-			new SelectionDropAdapter(new TransferAgent(
-				IDETransferId.NAV_SELECTION_TRANSFER, LocalSelectionTransfer
-					.getTransfer(), true) {
+		transferDropTargetTable.put(Integer.valueOf(IDETransferId.NAV_SELECTION_TRANSFER.hashCode()),
+				new SelectionDropAdapter(new TransferAgent(IDETransferId.NAV_SELECTION_TRANSFER,
+						LocalSelectionTransfer.getTransfer(), true) {
 
-				/*
-				 * (non-Javadoc)
-				 * 
-				 * @see org.eclipse.gmf.runtime.common.ui.services.dnd.core.ITransferAgent#getSelection(org.eclipse.swt.dnd.TransferData)
-				 */
-				public ISelection getSelection(TransferData transferData) {
-					return LocalSelectionTransfer.getTransfer().getSelection();
-				}
-			}));
+					@Override
+					public ISelection getSelection(TransferData transferData) {
+						return LocalSelectionTransfer.getTransfer().getSelection();
+					}
+				}));
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.gmf.runtime.common.ui.services.dnd.core.ITransferAdapterProvider#getTransferDragSourceAdapter(java.lang.String)
-	 */
-	public ITransferDragSourceListener getTransferDragSourceAdapter(
-			String transferId) {
-		assert null != transferId : "transferId cannot be null";; //$NON-NLS-1$
-
-		ITransferDragSourceListener listener = null;
-		listener = (ITransferDragSourceListener) getTransferDragSourceTable()
-			.get(Integer.valueOf(transferId.hashCode()));
-		return listener;
+	@Override
+	public ITransferDragSourceListener getTransferDragSourceAdapter(String transferId) {
+		return transferDragSourceTable.get(Integer.valueOf(Objects.requireNonNull(transferId, "transferId cannot be null").hashCode())); //$NON-NLS-1$
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.gmf.runtime.common.ui.services.dnd.core.ITransferAdapterProvider#getTransferDropTargetAdapter(java.lang.String)
-	 */
-	public ITransferDropTargetListener getTransferDropTargetAdapter(
-			String transferId) {
-		assert null != transferId : "transferId cannot be null"; //$NON-NLS-1$
-
-		ITransferDropTargetListener listener = null;
-		listener = (ITransferDropTargetListener) getTransferDropTargetTable()
-			.get(Integer.valueOf(transferId.hashCode()));
-		return listener;
+	@Override
+	public ITransferDropTargetListener getTransferDropTargetAdapter(String transferId) {
+		return transferDropTargetTable.get(Integer.valueOf(Objects.requireNonNull(transferId, "transferId cannot be null").hashCode())); //$NON-NLS-1$
 	}
 
-	/**
-	 * Return table of transfer drag source adapters
-	 * 
-	 * @return Hashtable
-	 */
-	private Hashtable getTransferDragSourceTable() {
-		return transferDragSourceTable;
-	}
-
-	/**
-	 * Return table of transfer drop target adapters
-	 * 
-	 * @return Hashtable
-	 */
-	private Hashtable getTransferDropTargetTable() {
-		return transferDropTargetTable;
-	}
 }
